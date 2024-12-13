774dc49039fe4b65c5ffc501ce7228d09747d868
nyee0110 <imoizi0110@gmail.com>
2024-12-13 22:17:34 +0900

    앱 브랜드 리스트

--- Follow_Me_App/app/src/main/java/com/example/follow_me/BrandListActivity.java
+++ Follow_Me_App/app/src/main/java/com/example/follow_me/BrandListActivity.java
@@ -2,8 +2,6 @@ package com.example.follow_me;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
